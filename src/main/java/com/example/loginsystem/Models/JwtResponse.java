97c3c09c2705e100b09b152030105e40ad83c042
Shishir Kumar Das <sshishirdas38@gmail.com>
2025-03-03 15:57:33 +0600

    Updating_Research_profile

--- src/main/java/com/example/loginsystem/Models/JwtResponse.java
+++ src/main/java/com/example/loginsystem/Models/JwtResponse.java
@@ -1,8 +1,12 @@
 package com.example.loginsystem.Models;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class JwtResponse {
     private String token;
     public JwtResponse(String token) { this.token = token; }
